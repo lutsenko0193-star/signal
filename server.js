d875c98c0b48601d28f8b6ec608c11aaa39b3402
lutsenko0193-star <lutsenko0193@gmail.com>
2026-05-31 14:09:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const http = require('http');
 const app = express();
-const server = https://https://signal-o6x2.onrender.com/;
+const server = http.createServer(app);
 const PORT = process.env.PORT || 3001;
 const HOST = '0.0.0.0';
 
